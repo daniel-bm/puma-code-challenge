4f870908cdd89de02d1c67b1bd4c9b3c34fb070f
Daniel Barcelos <danielbarcelos.7@hotmail.com>
2023-10-25 01:02:27 -0300

    feat: create user controller

---
+++ backend/controller/userController.js
@@ -0,0 +1,44 @@
+// controller/userController.js
+const userService = require('../service/userService');
+
+const addUser = async (req, res) => {
+  const { username, nome, avatar, url } = req.body;
+  const result = await userService.addUser({ username, nome, avatar, url });
+  if (result.error) {
+    res.status(400).json({ error: result.error });
+  } else {
+    res.status(201).json(result);
+  }
+};
+
+const getAllUsers = (req, res) => {
+  const users = userService.getAllUsers();
+  res.json(users);
+};
+
+const deleteUser = (req, res) => {
+  const { username } = req.params;
+  const result = userService.deleteUser(username);
+  if (result.error) {
+    res.status(400).json({ error: result.error });
+  } else {
+    res.status(200).json(result);
+  }
+};
+
+const toggleStar = (req, res) => {
+  const { username } = req.params;
+  const result = userService.toggleStar(username);
+  if (result.error) {
+    res.status(400).json({ error: result.error });
+  } else {
+    res.json(result);
+  }
+};
+
+module.exports = {
+  addUser,
+  getAllUsers,
+  deleteUser,
+  toggleStar,
+};
