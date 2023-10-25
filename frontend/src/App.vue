662d3e357d188fe503d48663bd8cd52d325bbd4d
Daniel Barcelos <danielbarcelos.7@hotmail.com>
2023-10-25 13:18:07 -0300

    initial frontend implementation

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <MainScreen/>
+</template>
+
+<script>
+import MainScreen from './pages/MainScreen.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainScreen
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
