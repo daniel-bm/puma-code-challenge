728992c85f7b4072004d581ea11fd8f02d201bc0
Daniel Barcelos <danielbarcelos.7@hotmail.com>
2023-10-25 13:18:52 -0300

    feat: sort button component

---
+++ frontend/src/components/SortButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <button @click="sortByName">Ordenar por Nome (A-Z)</button>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      sortByName() {
+        this.$emit('sortByName');
+      },
+    },
+  };
+  </script>
+  
+  <style>
+
+  button {
+    border-radius: 25px;
+    border: 1px solid #000;
+    background: #000;
+    color: white;
+    padding: 5px 10px;
+    cursor: pointer;
+    width: 250px;
+    height: 50px;
+    font-size: 16px;
+    padding: 0 20px;
+    transition: filter 0.2s;
+  }
+
+  button:hover {
+    background-color: #282828;
+  }
+
+  </style>
+  
